d82fc0a447957cf5d513246c96d095fce0c0c7af
Abdelrahman Sedky <abdelrahmanoda@gmail.com>
2021-08-31 18:01:29 +0200

    CardData CardAdapter

---
+++ app/src/main/java/com/example/tripawy/CardAdapter.java
@@ -0,0 +1,97 @@
+package com.example.tripawy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.Viewholder> {
+
+    private Context context;
+    private ArrayList<CardData> cardDataArrayList;
+
+    public CardAdapter(Context context, ArrayList<CardData> cardDataArrayList) {
+        this.context = context;
+        this.cardDataArrayList = cardDataArrayList;
+    }
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+
+        return new Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+        CardData data = cardDataArrayList.get(position);
+        holder.getTxtName().setText(data.getTripName());
+        holder.getTxtDate().setText(data.getDate().toString());
+        holder.getTxtTime().setText(data.getTime().toString());
+        holder.getTxtFrom().setText(data.getFrom());
+        holder.getTxtTo().setText(data.getTo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+        private View itemView;
+        private TextView txtDate;
+        private TextView txtTime;
+        private TextView txtName;
+        private TextView txtFrom;
+        private TextView txtTo;
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView=itemView;
+        }
+
+        public TextView getTxtName() {
+            if (txtName == null) {
+                txtName = itemView.findViewById(R.id.txtName);
+            }
+            return txtName;
+        }
+
+        public TextView getTxtDate() {
+            if (txtDate == null) {
+                txtDate = itemView.findViewById(R.id.txtDate);
+            }
+            return txtDate;
+        }
+
+        public TextView getTxtTime() {
+            if (txtTime == null) {
+                txtTime = itemView.findViewById(R.id.txtTime);
+            }
+            return txtTime;
+        }
+
+        public TextView getTxtFrom() {
+            if (txtFrom == null) {
+                txtFrom = itemView.findViewById(R.id.txtFrom);
+            }
+            return txtFrom;
+        }
+
+        public TextView getTxtTo() {
+            if (txtTo == null) {
+                txtTo = itemView.findViewById(R.id.txtTo);
+            }
+            return txtTo;
+        }
+
+
+    }
+}
