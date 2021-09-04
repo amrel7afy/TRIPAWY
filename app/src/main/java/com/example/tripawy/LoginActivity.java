f357cec70caf3d138c5e04c28c2c2e189cee931c
amrel7afy <amr.el7afy@gmail.com>
2021-09-04 18:47:52 +0200

    add login Activity

---
+++ app/src/main/java/com/example/tripawy/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.tripawy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login=findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
